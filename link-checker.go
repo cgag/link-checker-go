7348ea244655ed0ad4a2ffd50ab68e92aa2f90a8
Curtis Gagliardi <curtis@curtis.io>
2014-12-21 18:32:36 -0800

    Basic parallel version, pauses on every level of crawl depth however.

--- link-checker.go
+++ link-checker.go
@@ -6,11 +6,16 @@ import (
 	"log"
 	"net/http"
 	u "net/url"
+	// "time"
 )
 
+// var totalTime = time.ParseDuration("0ms")
+
 func main() {
-	seedUrl, err := u.Parse("http://devotter.com")
+	// seedUrl, err := u.Parse("http://devotter.com")
 	// seedUrl, err := u.Parse("http://curtis.io")
+	// seedUrl, err := u.Parse("http://me.justin.sh")
+	seedUrl, err := u.Parse("https://ocharles.org.uk")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -31,27 +36,39 @@ type TestedUrl struct {
 	linkedUrls []u.URL
 }
 
-func FindLinks(url u.URL) (TestedUrl, error) {
-	res, err := http.Get(url.String())
-	if err != nil {
-		return TestedUrl{}, err
-	}
+func FindLinks(url u.URL) (chan TestedUrl, chan error) {
+	c := make(chan TestedUrl)
+	errChan := make(chan error)
 
-	doc, err := goquery.NewDocumentFromResponse(res)
-	if err != nil {
-		return TestedUrl{}, err
-	}
-
-	linkedUrls := make([]u.URL, 0)
-	doc.Find("a").Each(func(_ int, sel *goquery.Selection) {
-		href, _ := sel.Attr("href")
-		parsed, err := u.Parse(href)
+	go func() {
+		// t1 := time.Now()
+		res, err := http.Get(url.String())
 		if err != nil {
-			return
+			errChan <- err
 		}
-		linkedUrls = append(linkedUrls, *url.ResolveReference(parsed))
-	})
-	return TestedUrl{url, res.StatusCode, linkedUrls}, nil
+		// delta := time.Since(t1)
+		// totalTime += delta
+		// fmt.Println("fetched in: ", delta)
+
+		doc, err := goquery.NewDocumentFromResponse(res)
+		if err != nil {
+			errChan <- err
+		}
+
+		linkedUrls := make([]u.URL, 0)
+		doc.Find("a").Each(func(_ int, sel *goquery.Selection) {
+			href, _ := sel.Attr("href")
+			parsed, err := u.Parse(href)
+			if err != nil {
+				errChan <- err
+			}
+			linkedUrls = append(linkedUrls, *url.ResolveReference(parsed))
+		})
+		c <- TestedUrl{url, res.StatusCode, linkedUrls}
+		close(c)
+	}()
+
+	return c, errChan
 }
 
 func crawl(seedUrl u.URL) chan TestedUrl {
@@ -59,40 +76,60 @@ func crawl(seedUrl u.URL) chan TestedUrl {
 	go func() {
 		crawled := make(map[u.URL]bool)
 
-		fmt.Println("fetching first url")
-		tested, err := FindLinks(seedUrl)
-		fmt.Println("fetched first url")
-		if err != nil {
+		testedChan, errChan := FindLinks(seedUrl)
+
+		var tested TestedUrl
+		select {
+		case t := <-testedChan:
+			tested = t
+			break
+		case err := <-errChan:
 			log.Fatal(err)
 		}
+
 		crawled[seedUrl] = true
-		fmt.Println("pushing to output")
 		retUrls <- tested
 
 		toTest := make([]u.URL, 0)
 		toTest = tested.linkedUrls
 
 		for {
-			fmt.Println("in crawl loop")
 			nextRound := make([]u.URL, 0)
+
+			childChans := make([]chan TestedUrl, 0)
+			errChans := make([]chan error, 0)
+
 			for _, child := range toTest {
 				if _, alreadyCrawled := crawled[child]; !alreadyCrawled {
-					testedChild, err := FindLinks(child)
-					if err != nil {
-						continue
-					}
 					crawled[child] = true
-					retUrls <- testedChild
-					if shouldCrawl(seedUrl, child) {
-						nextRound = append(nextRound, testedChild.linkedUrls...)
-					}
+					testedChildChan, errChan := FindLinks(child)
+					childChans = append(childChans, testedChildChan)
+					errChans = append(errChans, errChan)
 				}
 			}
+			fmt.Println("done making chans: ", len(childChans))
+
+			for i := range childChans {
+				c, ec := childChans[i], errChans[i]
+
+				select {
+				case testedChild := <-c:
+					retUrls <- testedChild
+					if shouldCrawl(seedUrl, testedChild.url) {
+						nextRound = append(nextRound, testedChild.linkedUrls...)
+					}
+				case <-ec:
+					continue
+				}
+			}
+
 			if len(nextRound) == 0 {
 				break
 			}
+
 			toTest = nextRound
 		}
+
 		close(retUrls)
 	}()
 
